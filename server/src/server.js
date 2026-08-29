d3b3651c9e2cd4b7623962f2b6ea71e2794379a2
mustafiz46607 <mustafizjr16@gmail.com>
2026-08-29 13:00:45 +0600

    connect MongoDB to Express

--- server/src/server.js
+++ server/src/server.js
@@ -2,22 +2,23 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
+
 app.use(cors());
 app.use(express.json());
 
-// Test route
 app.get("/", (req, res) => {
   res.json({
     message: "TaskFlow API is running",
   });
 });
 
-// Start server
 app.listen(PORT, () => {
-  console.log(`TaskFlow server running on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
